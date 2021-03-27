6535791421cea59aa7c315973e7721a5b251d76a
ilyaReart <ilyareart@gmail.com>
2021-03-27 14:19:12 +0300

    middle

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -6,9 +6,12 @@ import { API_KEY } from "../API/API_KEY";
 export const Main: React.FC = () => {
   const [isImages, setImages] = useState<any>("");
   const [tagName, setTagName] = useState<string>("");
+  const [views, setViews] = useState<any>([]);
+  const [counter, setCounter] = useState<number>(0);
 
   const onClickDownload = (e: any) => {
     e.preventDefault();
+
     const response = getResponse(
       `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=${tagName}`
     );
@@ -50,11 +53,6 @@ export const Main: React.FC = () => {
         </button>
         <button className="groupButton">Группировать</button>
       </div>
-      <Content isImages={isImages} />
-      <Content isImages={isImages} />
-      <Content isImages={isImages} />
-      <Content isImages={isImages} />
-      <Content isImages={isImages} />
       <Content isImages={isImages} />
     </>
   );
